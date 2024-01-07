01e4f3d352567647f07c612908e7fcf06c5dee47
EntityKunG <methasitpan@gmail.com>
2024-01-08 00:09:38 +0700

    test success

--- test-pexpect.py
+++ test-pexpect.py
@@ -3,26 +3,19 @@ import pexpect
 
 def main():
     """pexpect"""
-    promt = '#'
+    PROMPT = '#'
     ip = '172.31.109.1'
     username = 'admin'
     password = 'cisco'
     command = 'show ip int brief'
-
-    # child = pexpect.spawn('telnet ' + ip)
-    # child = pexpect.spawn('telnet ' + ip, encoding='utf-8')
     child = pexpect.spawn('telnet ' + ip)
-    # pexpect.popen_spawn.PopenSpawn
-    # child = pexpect.popen_spawn.PopenSpawn('telnet ' + ip)
-    # child = pexpect.popen_spawn.PopenSpawn('telnet ' + ip, timeout=10)
-    
     child.expect('Username:')
     child.sendline(username)
     child.expect('Password:')
     child.sendline(password)
-    child.expect(promt)
+    child.expect(PROMPT)
     child.sendline(command)
-    child.expect(promt)
+    child.expect(PROMPT)
     result = child.before
     print(result)
     print()
