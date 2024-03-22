aaa750e7e29e3c333e7bc61c93f6ecbca22dd420
EntityKunG <64070215@kmitl.ac.th>
2024-03-22 13:15:35 +0700

    test-module-out

--- week15/s3.tf
+++ week15/s3.tf
@@ -2,6 +2,7 @@
 # RESOURCES
 ##################################################################################
 
+
 #Random ID
 resource "random_integer" "rand" {
   min = 100
@@ -10,13 +11,22 @@ resource "random_integer" "rand" {
 
 module "s3_bucket" {
   source          = "./modules"
-  bucket_name     = "pan-npa2024-${random_integer.rand.result}"
+  bucket_name     = "pan-npa2024-pj9gtdtrp"
   tags = merge(local.default_tags,{Name = "${var.default_name}-S3-Bucket"})
 }
 
-resource "aws_s3_object" "image" {
-  count = 2
-  bucket = module.s3_bucket.id
-  key    = "${count.index + 1}.jpg"
-  source = "./images/${count.index + 1}.png" 
+module "s3-bucket_object" {
+  source  = "terraform-aws-modules/s3-bucket/aws//modules/object"
+  version = "4.1.1"
+  bucket = "pan-npa2024-pj9gtdtrp"
+  tags = merge(local.default_tags,{Name = "${var.default_name}-S3-Bucket"})
+  key = "1.jpg"
+  file_source = "./images/1.png"
 }
+
+# resource "aws_s3_object" "image" {
+#   count = 2
+#   bucket = module.s3_bucket.id
+#   key    = "${count.index + 1}.jpg"
+#   source = "./images/${count.index + 1}.png" 
+# }
--- week15/variable.tf
+++ week15/variable.tf
@@ -50,7 +50,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.37.0"
+      version = "5.24"
     }
   }
 }
